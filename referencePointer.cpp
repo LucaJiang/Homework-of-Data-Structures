1895be365dadb8a278600248de81c9a34a006a29
Jiang W.X <jiangwx7@mail2.sysu.edu.cn>
2019-07-25 20:11:41 +0800

    Add files via upload

---
+++ referencePointer.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+struct str
+{
+	int data;
+	str*next;
+};
+
+void fun(str*&p1, str*p2, str*p3)
+{
+	cout << "\nIn the function:\n";
+	cout << "address of p1:" << &p1 << endl;
+	cout << "data of p1:" << p1->data << endl;
+	cout << "address of p1->next:" << p1->next << endl;
+	cout << "address of p2:" << &p2 << endl;
+	cout << "p2 point to:" << p2 << endl;
+	cout << "change data in function!\n\n";
+	p1->data = 3;
+	p2->data = 4;
+//	p3->next->data = 5;
+	return;
+}
+
+int main()
+{
+	str s1, s2;
+	str*p1 = &s1;
+	str*p2 = &s2;
+	s1.data = 1;
+	s2.data = 2;
+	s1.next = &s2;//p2
+	s2.next = NULL;
+	cout << "address of p1:" << &p1 << endl;
+	cout << "p1 point to:" << p1 << endl << endl;
+	cout << "p1->next:" << p1->next << endl;
+	cout << "address of p2:" << &p2 << endl;
+	cout << "p2 point to:" << p2 << endl;
+	fun(p1, p2, p1);
+	cout << "In main:\n";
+	cout << "p1->data:" << p1->data << " p2->data:" << p2->data << endl;
+	system("pause");
+	return 0;
+}
